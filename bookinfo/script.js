647227c64c5f63693725947fd0c6c46cb4bfab2d
kuwabara <kyuwabara@users.noreply.github.com>
2023-08-11 13:10:38 +0900

    Update script.js

--- bookinfo/script.js
+++ bookinfo/script.js
@@ -47,6 +47,8 @@ javascript: {
   // 表示する内容
   const lines = `---\ntitle: ${title} \nauthor: [${authors.join(',')}]\nasin: ${asin}\npublish_date: ${publish_date}\n---\n# ${link}\n${viewAuthors.join('\n')}${mdimage}\n${selection}\n\n----\n`;
   
-  document.getElementById('bookDescription_feature_div').innerHTML = `<textarea style="height:500px">${lines}</textarea>`;
-  window.open('obsidian://new?vault=vaultone&name='+encodeURIComponent(title.trim())+'&content='+encodeURIComponent(body));
+  //document.getElementById('bookDescription_feature_div').innerHTML = `<textarea style="height:500px">${lines}</textarea>`;
+  //window.open('obsidian://new?vault=vaultone&name='+encodeURIComponent(title.trim())+'&content='+encodeURIComponent(body));
+  const oburi = 'obsidian://advanced-uri?vault=vaultone&filepath='+title+ '&data='+lines;
+  window.open(oburi);
 }
